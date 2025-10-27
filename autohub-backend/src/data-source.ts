bc1fea5fc94c4ea90464e04a3941279256ce32c6
yeracode11 <ersul143@gmail.com>
2025-10-27 18:37:18 +0500

    migrations2

--- autohub-backend/src/data-source.ts
+++ autohub-backend/src/data-source.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
     host: process.env.DB_HOST || 'localhost',
     port: Number(process.env.DB_PORT) || 5432,
     username: process.env.DB_USER || 'eracode',
-    password: process.env.DB_PASSWORD || '',
+    password: process.env.DB_PASSWORD || 'erasoft123',
     database: process.env.DB_NAME || 'autohubdb', // <- обязательно autohubdb
     synchronize: false,
     logging: false,
